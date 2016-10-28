2be555d076be17b7a712889b99344232ed9b2c4d
pat-e <paternst@gmail.com>
2016-10-28 18:46:07 +0200

    Create vid_tran_inst.sh

---
+++ vid_tran_inst.sh
@@ -0,0 +1,13 @@
+# much cleanup needed, first draft to collect the neeeded packages
+
+sudo apt-get update
+
+sudo add-apt-repository ppa:stebbins/handbrake-releases
+sudo apt-get update
+sudo apt-get install handbrake-cli
+
+sudo apt-get install mkvtoolnix
+
+sudo apt-get install install mplayer
+
+sudo apt-get install mp4v2-utils
