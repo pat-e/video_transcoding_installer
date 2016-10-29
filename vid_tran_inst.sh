a3e1059f9fbf69deba9836a360c4a3c0eeb21d1e
pat-e <paternst@gmail.com>
2016-10-29 14:58:07 +0200

    Update vid_tran_inst.sh

--- vid_tran_inst.sh
+++ vid_tran_inst.sh
@@ -2,12 +2,13 @@
 
 sudo apt-get update
 
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:stebbins/handbrake-releases
 sudo apt-get update
 sudo apt-get install handbrake-cli
 
 sudo apt-get install mkvtoolnix
 
-sudo apt-get install install mplayer
+sudo apt-get install mplayer
 
 sudo apt-get install mp4v2-utils
